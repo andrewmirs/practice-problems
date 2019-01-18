e875cd7176505568d393993bfc0c5088d5fa42d1
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-01-18 15:15:41 -0800

    finished drillset 3 except for problem 2

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,21 +1,46 @@
 
 
-function getPath(){
-
+function getPath(string){
+    return string.split('/');
 }
 
-function getPathParts(){
-
+function getPathParts(url){
+    return url.split(":");
 }
 
-function getCapitalCount(){
-
+function getCapitalCount(array){
+    count = 0;
+    for ( var i=0; i<array.length; i++){
+        if(array[i].charCodeAt(0) < 91){
+            count ++
+        }
+    }
+    return count;
 }
 
-function correctCalcChecker(){
+function correctCalcChecker(equations){
+    var answer = [];
 
+    for (var i=0; i<equations.length; i++){
+        if(doMath(equations[i].num1,equations[i].num2,equations[i].op) === equations[i].result){
+            answer.push(equations[i]);
+        }
+    }
+
+    return answer;
 }
 
-function doMath(){
-
+function doMath(num1, num2, op){
+    if(op === "+"){
+        return num1 + num2;
+    }
+    if(op === "-"){
+        return num1 - num2;
+    }
+    if (op === "*"){
+        return num1 * num2;
+    }
+    if (op === "/"){
+        return num1 / num2;
+    }
 }
