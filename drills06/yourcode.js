04bfe26ba44aa5c6087288a1413c3d06d57a3524
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-01-28 20:38:01 -0800

    finished drills6

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,29 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement( element, className ){
+    $(element).removeClass(className);
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement( element, className ){
+    $(element).toggleClass(className);
 }
 
-function hideElements(  ){
-	
+function hideElements( element, removalType ){
+    if(removalType === "hide"){
+        $(element).hide();
+    } else if (removalType === "removeChildren"){
+        $(element).empty();
+    } else if (removalType === "removeSelf"){
+        $(element).remove();
+    }
 }
 
-function addAttributeToElement( ){
-	
+function addAttributeToElement( element, attr, val ){
+    $(element).setAttribute(attr, val);
 }
 
-function putPosInElement(  ){
-	
+function putPosInElement( element, attr, val ){
+	$(element).setAttribute(attr, val);
 }
 
 
