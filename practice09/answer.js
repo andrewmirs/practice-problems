cb67e8190878f7a915e151c802a3aa81676be3ba
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-03-25 13:58:08 -0700

    finished fitting words practice problem

---
+++ practice09/answer.js
@@ -0,0 +1,24 @@
+function fittingWords(str, words){
+  var split = str.split('');
+  var answer = [];
+  
+  for( var i=0; i< words.length; i++ ){
+    for( var ltrs=0; ltrs < split.length; ltrs++ ){
+      if(!words[i].includes(split[ltrs])){
+//           console.log(split[ltrs] + ' doesnt exist in '  + words[i])
+        i++;
+        break;
+      }
+    }
+    if(i < words.length){
+      answer.push(words[i]);
+    }
+  }
+  
+  return answer;
+}
+
+var myArray = ['cat','caterpillar','whale','accurate','smile','cactus','cute'];
+
+console.log(fittingWords('cat', myArray));
+  
