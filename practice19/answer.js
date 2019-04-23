1f334b107b123cefea3cd5c6b5abfaa02ecdc424
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-04-23 10:42:20 -0700

    finished a few practice problems

--- practice19/answer.js
+++ practice19/answer.js
@@ -1,1 +1,16 @@
-answer.js
+
+function sort_object_list_by_name(arr){
+  let answer = arr;
+  answer.sort((a,b) => a.age < b.age ? -1 : 1 );
+  return answer;
+}
+
+var input_array = 
+[
+    {name: 'Michael',age:40, gender: 'M'},
+	{name: 'John', age:42, gender:'M'},
+	{name: 'Jack', age:34, gender:'M'},
+	{name: 'Kendra',age:48, gender:'F'}
+];
+
+console.log(sort_object_list_by_name(input_array));
