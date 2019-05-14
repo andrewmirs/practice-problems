5ffc0305b11ba0dc9d22fcea0d1f06bb9ad44146
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-05-14 15:36:05 -0700

    practice problems 21 - 23 complete

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,8 @@
-answer.js
+function bipolarLoop(){
+    for(let i=0, x=10; i<10, x>0; i++, x--){
+      console.log('i = ', i);
+      console.log('x = ', x);
+    }
+  }
+  
+  console.log(bipolarLoop());
